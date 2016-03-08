f1650656c0492be557038bf97b6077166c0fe1a3
aglv <alessandro.volz@gmail.com>
2016-03-08 09:05:24 +0100

    wiki attempts

--- wiki.sh
+++ wiki.sh
@@ -8,17 +8,21 @@ doxygen Doxyfile
 rm -Rf doc/md
 cp -Rf doc/xml doc/md
 
+echo "Warning: pandoc XML to MD conversion doesn't work properly for us..."
+
 cd doc/md
 find . -name '*.xml' | while read xml; do
 	pandoc -f docbook -t markdown -s "$xml" -o "${xml%.*}.md"
 	rm "$xml"
 done
 
+exit 1
+
 cd "$d"
 rm -rf wiki/*
 cp -Rf build/libopc/doc/md/* wiki/
 
 cd wiki
-mv index.md Home.md
+mv indexpage.md Home.md
 
 exit 0
