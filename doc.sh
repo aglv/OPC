c5f702385a8a3881b3dabfb886a4664c8e84d777
aglv <alessandro.volz@gmail.com>
2016-03-12 17:18:18 +0100

    subs

--- doc.sh
+++ doc.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
+set -e
+
 d=`pwd`
+
+if [ ! -f doc/index.html ] ; then
+	cd doc
+	git submodule update --init
+	cd ..
+fi
+
+if [ ! -f libopc/configure ] ; then
+	cd libopc
+	git submodule update --init
+	cd ..
+fi
+
+exit 0
+
+rm -Rf build/libopc
+cp -af libopc build/
 
 cd build/libopc
 doxygen Doxyfile
 
-rm -Rf doc/md
+exit 0
+
+cd build/libopc/doc/
+
 cp -Rf doc/html doc/md
 
 echo "Warning: pandoc XML/HTML to MD conversion doesn't work properly for us..."
